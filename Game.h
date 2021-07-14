49ce319c530e6b667dba208f525884a1715f93da
xLimi <64392332+xLimi@users.noreply.github.com>
2021-07-14 15:50:35 +0900

    add variances about Game Frame
    add variances about game frame position (X, Y)

--- Game.h
+++ Game.h
@@ -1,14 +1,36 @@
 #pragma once
 
-#define Test 0
+#define easy "Easy"      //難易度名 Easy
+#define normal "Normal"  //難易度名 Normal
+#define hard "Hard"      //難易度名 Hard
+#define Omino_num 1               //Oミノの出現番号
+#define Smino_num 2               //Sミノの出現番号
+#define Jmino_num 4               //Jミノの出現番号
+#define Lmino_num 5               //Lミノの出現番号
+#define Tmino_num 7               //Tミノの出現番号
+#define Frame_border 3            //ゲーム画面の枠の太さ
+#define Frame_leftX 44            //ゲーム画面枠の左端 
+#define Frame_width 10            //ゲーム画面の幅
+#define Frame_rightX 60           //ゲーム画面の右端
+#define Frame_upY 5               //ゲーム画面の上端
+#define Frame_height 20           //ゲーム画面の高さ
+#define Frame_downY 25            //ゲーム画面の下端
+#define Next_frame_leftX 70       //次のミノを表示する箱の左端
+#define Next_frame_rightX 76      //次のミノを表示する箱の右端
+#define Next_frame_upY 15         //次のミノを表示する箱の上端
+#define Next_frame_downY 22       //次のミノを表示する箱の下端
+#define Ini_data_X 90          //プレイヤー名、難易度の表示開始X座標
+#define Ini_data_Y 5           //プレイヤー名、難易度の表示開始Y座標
+
+
 
 //ゲーム進行、結果に必要なデータの格納
 struct Game {
 	char plar_name[CHARBUFF];    //プレイヤーの名前
 	char dif_level[CHARBUFF];    //難易度名
-	int fall_speed;              //ミノの落下スピード
-	int Imino_rate;              //Iミノの出現確率
-	int SZmino_rate;             //SミノとZミノの出現確率
+	double fall_speed;              //ミノの落下スピード
+	int Imino_num;               //Iミノの出現番号
+	int Zmino_num;               //Zミノの出現番号
 	int result_row;              //消去した行数
 	int Imino_cnt;               //Iミノを操作した数
 	int Omino_cnt;               //Oミノを操作した数
@@ -19,6 +41,6 @@ struct Game {
 	int Tmino_cnt;               //Tミノを操作した数
 };
 
-void setDifficulty(Game data);    //難易度による設定
-void makeGameFrame(Game data);            //ゲーム画面の枠を描画
-void outputResult(const char* fileName, int result, Game data);  //結果の出力
+void setDifficulty(Game *data);    //難易度による設定
+void makeFrame(int height, int width);   //ゲーム画面枠の描画
+void outputResult(const char* fileName, Game data);  //結果の出力
